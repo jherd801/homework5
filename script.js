42e2a4b76ef12be422c821815df95c3be1c11bbc
jherd801 <jherd801@gmail.com>
2020-03-29 06:59:10 -0600

    fixed overlapping row issue by removing 80px row height

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let currentDay = document.getElementById("currentDay");
 let currentTime = document.getElementById("currentTime");
 let m = moment();
-let btn9 = document.getElementById("btn9");
-let btn10 = document.getElementById("btn10");
+let btn9 = $("#btn9");
+let btn10 = $("#btn10");
 let btn11 = document.getElementById("btn11");
 let btn12 = document.getElementById("btn12");
 let btn13 = document.getElementById("btn13");
@@ -13,7 +13,7 @@ let btn16 = document.getElementById("btn16");
 let btn17 = document.getElementById("btn17");
 // Task input fields
 let inp9 = document.getElementById("inp9");
-let inp10
+let inp10 = document.getElementById("inp10");
 let inp11
 let inp12
 let inp13
@@ -22,7 +22,7 @@ let inp15
 let inp16
 let inp17
 
-
+// let buttonClickedID = this.parentNode.previousSibling.firstElementChild.id
 // Parsing moment.js object
 const mHour = m.format("hh");
 const date = m.format("dddd, MMMM Do YYYY");
@@ -36,25 +36,22 @@ currentTime.innerText = "Current time is " + " " + time;
 
 // Add items to local storage
 let task9 = localStorage.getItem("task9");
-
 inp9.textContent = task9;
+let task10 = localStorage.getItem("task10");
+inp10.textContent = task10;
 
-// btn9.addEventListener("click", function() {
-//     let task9 = inp9.value;   
-//     localStorage.setItem("task9", task9);
-// });
 
-button.addEventListener("click", function(clicked_id) {
-
-    console.log(clicked_id);
+$("button").on("click", function() {
+    let task9 = inp9.value;   
+    localStorage.setItem("task9", task9);
+    let task10 = inp10.value;   
+    localStorage.setItem("task10", task10);
     console.log(this);
     console.log(this.parentNode.previousSibling);
     console.log(this.parentNode.previousSibling.firstElementChild.id);
     console.log(this.parentNode.previousSibling.firstElementChild.value);
     console.log(this.id);
-})
-
-// Write function to get id from element
+});
 
 
 // Apply style to highlight if time block is in past, present, or future
